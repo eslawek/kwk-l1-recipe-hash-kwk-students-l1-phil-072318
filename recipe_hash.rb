136d730f5fe3edcbe28927b688531fbc0f96147d
eslawek <ellieslawek@icloud.com>
2018-07-26 02:57:13 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -50,9 +50,7 @@ HASH = {:almond_flour=>"2 cups", :gluten_freewhole_oats=>"1 cup", :kosher_salt=>
 def amount_of_chocolate_chips  #7
    cookies = {:almond_flour=>"2 cups", :gluten_freewhole_oats=>"1 cup", :kosher_salt=>"1/2 teaspoon", :baking_powder=>"1/2 teaspoon", :baking_soda=>"1/4 teaspoon", :Xanthan_Gum=>"1/2 teaspoon", :slivered_almonds=>"1/4 cup", :mini_dark_chocolate_chips=>"3/4 cup", :olive_oil=>"1/2 cup", :agave=>"1/4 cup"}
     
-    amount_choco = cookies[7]
-  cookies.each do |amount_choco|
-  puts "#{amount_choco}"
+    puts cookies[7]
 
   end
 end
