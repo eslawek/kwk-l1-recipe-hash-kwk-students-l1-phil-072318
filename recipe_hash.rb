a841fad0c2af279c84d79ef7fa0fed8ac6fda2ce
eslawek <ellieslawek@icloud.com>
2018-07-26 02:49:44 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -49,7 +49,7 @@ recipe_ingredients()
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
 def amount_of_chocolate_chips(chocolate)  #7
    cookies = {:almond_flour=>"2 cups", :gluten_freewhole_oats=>"1 cup", :kosher_salt=>"1/2 teaspoon", :baking_powder=>"1/2 teaspoon", :baking_soda=>"1/4 teaspoon", :Xanthan_Gum=>"1/2 teaspoon", :slivered_almonds=>"1/4 cup", :mini_dark_chocolate_chips=>"3/4 cup", :olive_oil=>"1/2 cup", :agave=>"1/4 cup"}
-  
+    chocolate = cookies[7]
     food = cookies[7].keys
     number = cookies[7].values
     
