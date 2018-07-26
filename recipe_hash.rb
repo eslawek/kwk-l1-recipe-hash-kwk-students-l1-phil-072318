db91292d369f043d04afde0db98b86c5a02e1d20
eslawek <ellieslawek@icloud.com>
2018-07-26 02:32:49 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -31,7 +31,8 @@ end
 recipe
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
-def recipe_ingredients(perfect_10_recipe)
+
+def recipe_ingredients
 
   perfect_10_recipe_hash = {}
   index = 0
@@ -41,12 +42,15 @@ def recipe_ingredients(perfect_10_recipe)
     index += 1
   end
   return perfect_10_recipe_hash
-  
-  return 
 end
+
+recipe_ingredients()
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
 def amount_of_chocolate_chips(perfect_10_recipe)
+  
+  
+  
 end
 
 #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
