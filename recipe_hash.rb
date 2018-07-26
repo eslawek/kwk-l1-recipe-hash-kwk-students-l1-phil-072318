6ba1bef220b780821ac76fd86b7cddbb7e0744a9
eslawek <ellieslawek@icloud.com>
2018-07-26 03:10:30 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -96,6 +96,6 @@ def number_ingredients(perfect_10_recipe)
   
   cookies = {:almond_flour=>"2 cups", :gluten_freewhole_oats=>"1 cup", :kosher_salt=>"1/2 teaspoon", :baking_powder=>"1/2 teaspoon", :baking_soda=>"1/4 teaspoon", :Xanthan_Gum=>"1/2 teaspoon", :slivered_almonds=>"1/4 cup", :mini_dark_chocolate_chips=>"3/4 cup", :olive_oil=>"1/2 cup", :agave=>"1/4 cup"}
   
-  puts cookies.count
+  puts cookies.length
   
 end
